4081126a032f6fe950e8ea7244b5446b2947eb9b
Luis Zabala <luis.zabala076@gmail.com>
2019-01-30 02:41:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,16 @@
 class MP3Importer
-  
-  attr_accessor :name, :filepath, :artist, :genre, :title
-  
-  @@all = []
-  
-  def initialize(name)
-    @name = name
-  end
-  
+  attr_reader :path
 
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+  end
+
+  def import
+    files.each{|f| Song.new_by_filename(f)}
+  end
+end
 end
